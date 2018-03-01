75f33344cb7d970127dd8063afc0a0984dcf17be
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2018-03-01 17:51:37 +0000

    Removed github setting in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.ifconfig']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
